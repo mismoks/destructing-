ce6040103cfe08d1359bb9b7c9431303f919be16
mismoks <124860377+mismoks@users.noreply.github.com>
2023-07-19 00:56:44 +0530

    Add files via upload

---
+++ nested.js
@@ -0,0 +1,38 @@
+/*
+Write a function that takes an object representing a person as input, and extracts the name and street
+properties from a nested object using object destructuring. The function should return an object with these two
+properties. A sample object is given below.
+
+const person = {
+  name: "Pankaj",
+  age: 26,
+  address: {
+    street: "Ranchi Road",
+    city: "Ranchi",
+    state: "Jharkhnad",
+  },
+};
+
+*/
+
+const person = {
+    name: "Mokkshada Mishra",
+    age: 20,
+    address: {
+      street: "Parshurampur",
+      city: "Chandauli",
+      state: "Uttar Pradesh",
+    },
+  };
+  
+  // Code For destructuring
+  
+  function destruct({ name, address: { street } }) {
+    return { name, street };
+  }
+  
+  let { name, street } = destruct(person);
+  
+  console.log(name);
+  console.log(street);
+  
